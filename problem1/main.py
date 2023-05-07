1b3f546ce76c39137a19af08a47bb66d8c5b2419
karim rahimian <k.rahimian.av@gmail.com>
2023-05-08 01:16:49 +0330

    fix skip head

--- problem1/main.py
+++ problem1/main.py
@@ -1,13 +1,16 @@
 from Graph import Graph
 from CSV_Reader import CSV_Reader
+from GraphTraverser import GraphTraverser
 if __name__ =="__main__":
     filename = "data/graph.csv"
 
     reader = CSV_Reader(filename)
     node_names = reader.get_node_names()
     graph = Graph(node_names)
+
     reader.open_file()
-    reader.splip_head()
+    reader.skip_head()
+
     while (True):
         edge = reader.get_next_edge()
         if edge == None:
@@ -18,3 +21,15 @@ if __name__ =="__main__":
 
     graph.print_graph_beauty()
     graph.show_graphical()
+
+    operation =GraphTraverser()
+
+    print("BFS algorithm ")
+    tree_bfs = operation.bfs_traverse(graph)
+    operation.print_graph_beauty(tree_bfs)
+    operation.show_graphical(tree_bfs, graph.node_names, "BFS")
+
+    print ("DFS algorithm ")
+    tree_dfs = operation.dfs_traverse(graph)
+    operation.print_graph_beauty(tree_dfs)
+    operation.show_graphical(tree_dfs,graph.node_names,"DFS")
